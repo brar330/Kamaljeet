a66d2e23c09b7de3e249d58c47cc7debbde1fce5
brar330 <brar330@sheridancollege.ca>
2020-01-22 15:27:52 -0800

    initial project

---
+++ src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.*;
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         List a1 = new ArrayList();
+      a1.add("Krish");
+      a1.add("Good news");
+      a1.add("Jhalle");      
+      System.out.println(" ArrayList Elements");
+      System.out.print("\t" + a1);
+
+      List l1 = new LinkedList();
+      l1.add("Krish");
+      l1.add("Good news");
+      l1.add("Jhalle");
+      System.out.println();
+      System.out.println(" LinkedList Elements");
+      System.out.print("\t" + l1);
+   
+        // TODO code application logic here
+    }
+    
+}
